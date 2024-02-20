a253019460e58664730d2627d5a8263f95db107e
Joseph Wang <z5359179@ad.unsw.edu.au>
2024-02-21 01:27:36 +1100

    Finished stubbing for adminQuizNameUpdate

--- src/auth.js
+++ src/auth.js
@@ -26,12 +26,18 @@ function adminUserPasswordUpdate( authUserId, oldPassword, newPassword ) {
     }
 }
 
-function adminQuizzInfo ( authUserId, quizId ) {
+function adminQuizzInfo( authUserId, quizId ) {
     return {
         quizId: 1,
         name: 'My Quiz',
         timeCreated: 1683125870,
         timeLastEdited: 1683125871,
         description: 'This is my quiz',
+    }
+}
+
+function adminQuizNameUpdate( authUserId, quizId, description ) {
+    return { 
+        
     }
 }
