499921db421e245048062fbbd3af9d9ac0f6108a
Candice Chen <z5453581@ad.unsw.edu.au>
2024-02-21 10:10:54 +1100

    Done with adminAuthRegister and adminAuthLogin

---
+++ src/auth.js
@@ -0,0 +1,12 @@
+function adminAuthRegister(email, password, nameFirst, nameLast) {
+  return {
+    authUserId: 1
+  };
+}
+
+function adminAuthLogin(email, password) {
+  return {
+    authUserId: 1
+  };
+}
+
