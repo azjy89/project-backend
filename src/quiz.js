9ae6e19d7e45da6b96415cf0350f74869462e0fb
Put Your Name Here <z5480605@ad.unsw.edu.au>
2024-02-22 01:59:54 +1100

    Added documentation for quiz.js

--- src/quiz.js
+++ src/quiz.js
@@ -1,3 +1,10 @@
+/**
+ * Provide a list of all quizzes that are owned by the currently logged in user.
+ * 
+ * @param {int} authUserId 
+ * 
+ * @returns {object}
+ */
 function adminQuizList(authUserId) {
     return { 
         quizzes: [
@@ -9,18 +16,43 @@ function adminQuizList(authUserId) {
     }
 }
 
+/**
+ * Given basic details about a new quiz, create one for the logged in user.
+ * 
+ * @param {int} authUserId 
+ * @param {string} name 
+ * @param {string} description 
+ * 
+ * @returns {int}
+ */
 function adminQuizCreate(authUserId, name, description) {
     return {
         quizId: 2
     }
 }
 
+/**
+ * Given a particular quiz, permanently remove the quiz.
+ * 
+ * @param {int} authUserId 
+ * @param {int} quizId 
+ * 
+ * @returns {object}
+ */
 function adminQuizRemove(authUserId, quizId) {
     return {
         
     }
 }
 
+/**
+ * Get all of the relevant information about the current quiz.
+ * 
+ * @param {int} authUserId 
+ * @param {int} quizId 
+ * 
+ * @returns {object}
+ */
 function adminQuizInfo( authUserId, quizId ) {
     return {
         quizId: 1,
@@ -31,12 +63,30 @@ function adminQuizInfo( authUserId, quizId ) {
     }
 }
 
+/**
+ * Update the name of the relevant quiz.
+ * 
+ * @param {int} authUserId 
+ * @param {int} quizId 
+ * @param {string} name 
+ * 
+ * @returns {}
+ */
 function adminQuizNameUpdate( authUserId, quizId, name ) {
     return { 
 
     }
 }
 
+/**
+ * Update the description of the relevant quiz.
+ * 
+ * @param {int} authUserId 
+ * @param {int} quizId 
+ * @param {string} description 
+ * 
+ * @returns {}
+ */
 function adminQuizDescriptionUpdate( authUserId, quizId, description){
     return {
 
