e33ebde9576c6bbaf2383219c8a336742139c63f
Aidan <z5481380@ad.unsw.edu.au>
2024-02-21 14:32:15 +1100

    Finished stubbing for adminQuizDescriptionUpdate in auth.js

--- src/auth.js
+++ src/auth.js
@@ -41,3 +41,9 @@ function adminQuizNameUpdate( authUserId, quizId, description ) {
         
     }
 }
+
+function adminQuizDescriptionUpdate( authUserId, quizId, description){
+    return {
+
+    }
+}
