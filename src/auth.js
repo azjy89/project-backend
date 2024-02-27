ad433060bbe620ab38df36edd228f0aedad06f9b
Candice Chen <z5453581@ad.unsw.edu.au>
2024-02-27 23:30:51 +1100

    updated

--- src/auth.js
+++ src/auth.js
@@ -1,4 +1,14 @@
-
+/**
+  * Register a user with an email, password, and names, 
+  * then returns their authUserId value.
+  * 
+  * @param {string} email
+  * @param {string} password
+  * @param {string} nameFirst
+  * @param {string} nameLast
+  * 
+  * @returns {int}
+*/
 
 function adminAuthRegister(email, password, nameFirst, nameLast) {
   return {
@@ -6,11 +16,29 @@ function adminAuthRegister(email, password, nameFirst, nameLast) {
   }
 }
 
+/**
+ * Given a registered user's email and password returns their authUserId value.
+ * 
+ * @param {string} email 
+ * @param {string} password 
+ * 
+ * @returns {int}
+ */
+
 function adminAuthLogin(email, password) {
     return {
       authUserId: 1
     }
 }
+
+/**
+ * Given an admin user's authUserId, return details about the user. "name" is 
+ * the first and last name concatenated with a single space between them.
+ * 
+ * @param {int} authUserId 
+ * 
+ * @returns {object}
+ */
 
 function adminUserDetails( authUserId ) {
     return { user:
@@ -24,11 +52,34 @@ function adminUserDetails( authUserId ) {
     }
 }
 
+/**
+ * Given an admin user's authUserId and a set of properties, update the 
+ * properties of this logged in admin user.
+ * 
+ * @param {int} authUserId 
+ * @param {string} email 
+ * @param {string} nameFirst 
+ * @param {string} nameLast 
+ * 
+ * @returns {}
+ */
+
 function adminUserDetailsUpdate( authUserId, email, nameFirst, nameLast ) {
     return {
         
     }
 }
+
+/**
+ * Given details relating to a password change, update the password of a 
+ * logged in user.
+ * 
+ * @param {int} authUserId 
+ * @param {string} oldPassword 
+ * @param {string} newPassword
+ * 
+ * @returns {}
+ */
 
 function adminUserPasswordUpdate( authUserId, oldPassword, newPassword ) {
     return {
