741a0a30d6e002e2f42db21f7722fa5ebd557917
someguy <andrew.zhang0405@gmail.com>
2024-03-01 15:41:15 +1100

    Almost finished adminAuthRegister function in auth.js

--- src/auth.js
+++ src/auth.js
@@ -1,6 +1,18 @@
 import { getData, setData } from './dataStore.js';
 import isEmail from 'validator/es/lib/isEmail';
 
+// Global Variables
+const a = 97;
+const z = 122;
+const A = 65;
+const Z = 90;
+const hyphen = 45;
+const space = 35;
+const apostrophe = 39;
+const minNameLength = 2;
+const maxNameLength = 20;
+const minPasswordLength = 8;
+
 /**
   * Register a user with an email, password, and names, 
   * then returns their authUserId value.
@@ -15,10 +27,95 @@ import isEmail from 'validator/es/lib/isEmail';
 
 function adminAuthRegister(email, password, nameFirst, nameLast) {
   const data = getData();
-  
+  const result = adminAuthRegisterErrors(email, password, nameFirst, 
+                                         nameLast, data);
+  const newUser = new user    
+  data.users.push()
   return {
     authUserId: 1
   }
+}
+
+function adminAuthRegisterErrors(email, password, nameFirst, nameLast) {
+  if (!data.users.some(user => user.email === email)) {
+    return {
+      error: 'User with this email already exists'
+    }
+  }
+  if (!isEmail(email)) {
+    return {
+      error: 'Email is not valid'
+    }
+  }
+  if (!adminAuthRegisterValidNameCharacters(nameFirst)) {
+    return {
+      error: 'First name contains invalid characters'
+    }
+  }
+  if (!adminAuthRegisterValidNameCharacters(nameLast)) {
+    return {
+      error: 'Last name contains invalid characters'
+    }
+  }
+  if (!adminAuthRegisterValidNameLength(nameFirst)) {
+    return {
+      error: 'First name is too long or too short'
+    }
+  }
+  if (!adminAuthRegisterValidNameLength(nameLast)) {
+    return {
+      error: 'Last name is too long or too short'
+    }
+  }
+  if (password.length < minPasswordLength) {
+    return {
+      error: 'Password is too short'
+    }
+  }
+  if (!adminAuthRegisterValidPassword(password)) {
+    return {
+      error: 'Unsatisfactory password strength'
+    }
+  }
+  return {};
+}
+
+function adminAuthRegisterValidNameCharacters(name) {
+  for (let i = 0; i < structuredClone.length; i++) {
+    const charAscii = name.charCodeAt(i);
+    if (!((charAscii >= a && charAscii <= z) ||
+          (charAscii >= A && charAscii <= Z) ||
+          charAscii === hyphen ||
+          charAscii === apostrophe ||
+          charAscii === space)) {
+            return false;
+          }
+  }
+  return true;
+}
+
+function adminAuthRegisterValidNameLength(name) {
+  if (name.length < maxNameLength && name.length > minNameLength) {
+    return true;
+  }
+  return false;
+}
+
+function adminAuthRegisterValidPassword(password) {
+  let containsNumber = false;
+  let containsLetter = false;
+  for (const char of password) {
+    if (char.toLowerCase() !== char.toUpperCase()) {
+      containsLetter = true;
+    }
+    if (!isNaN(parseInt(char))) {
+      containsNumber = true;
+    }
+    if (containsNumber && containsLetter) {
+      return true;
+    }
+  }
+  return false;
 }
 
 /**
