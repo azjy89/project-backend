91ceb2cce8647820d5d5195250a226c67ebfb1c7
Andrew Zhang <andrewzhang@Andrews-MacBook-Pro.local>
2024-03-06 14:30:06 +1100

    Changed .userId to .authUserId where required as according to spec

--- src/quiz.test.js
+++ src/quiz.test.js
@@ -12,12 +12,12 @@ describe('adminQuizList', () => {
     beforeEach(() => {
         let user = adminAuthRegister('quiz@unsw.edu.au', 
         'abcd1234', 'Bobby', 'Dickens');
-        let quiz1 = adminQuizCreate(user.userId, 'COMP1531', 'Welcome!');
-        let quiz2 = adminQuizCreate(user.userId, 'asdfasdf', 'Welcome!');
+        let quiz1 = adminQuizCreate(user.authUserId, 'COMP1531', 'Welcome!');
+        let quiz2 = adminQuizCreate(user.authUserId, 'asdfasdf', 'Welcome!');
     });
 
     test('correct output of list of quizzes', () => {
-        expect(adminQuizList(user.userId)).toStrictEqual({
+        expect(adminQuizList(user.authUserId)).toStrictEqual({
             quizzes: [
                 {
                     quizId: quiz1.quizId,
@@ -31,9 +31,9 @@ describe('adminQuizList', () => {
         });
         let user1 = adminAuthRegister('quiz1@unsw.edu.au', 
         'abcd1234', 'Stephen', 'Robertson');
-        let quiz3 = adminQuizCreate(user1.userId, 'BOBBY', 'HELLO');
-        let quiz4 = adminQuizCreate(user1.userId, 'LOLLY', 'alksdjf');
-        expect(adminQuizList(user1.userId)).toStrictEqual({
+        let quiz3 = adminQuizCreate(user1.authUserId, 'BOBBY', 'HELLO');
+        let quiz4 = adminQuizCreate(user1.authUserId, 'LOLLY', 'alksdjf');
+        expect(adminQuizList(user1.authUserId)).toStrictEqual({
             quizzes: [
                 {
                     quizId: quiz3.quizId,
@@ -48,7 +48,7 @@ describe('adminQuizList', () => {
     });
     
     test('authUserId doesnt exist', () => {
-        expect(adminQuizList(user.userId + 1)).toStrictEqual( {error: expect.any(String)} );
+        expect(adminQuizList(user.authUserId + 1)).toStrictEqual( {error: expect.any(String)} );
     });
 
 });
@@ -59,14 +59,14 @@ describe('adminQuizCreate', () => {
     test('successful quiz creation', () => {
         let user = adminAuthRegister('quiz@unsw.edu.au', 
         'abcd1234', 'Bobby', 'Dickens');
-        let quiz = adminQuizCreate(user.userId, 'COMP1531', 'Welcome!');
+        let quiz = adminQuizCreate(user.authUserId, 'COMP1531', 'Welcome!');
         expect(quiz).toStrictEqual( {quizId: expect.any(Number)} );
     });
 
     test('authUserId doesnt exist', () => {
         let user = adminAuthRegister('quiz@unsw.edu.au', 
         'abcd1234', 'Bobby', 'Dickens');
-        let quiz = adminQuizCreate(user.userId + 1, 'COMP1531', 'Welcome!');
+        let quiz = adminQuizCreate(user.authUserId + 1, 'COMP1531', 'Welcome!');
         expect(quiz).toStrictEqual( {error: expect.any(String)} );
     });
 
@@ -83,15 +83,15 @@ describe('adminQuizCreate', () => {
     ])("checking name restrictions: '$name'", ({ name }) => {
         let user = adminAuthRegister('quiz@unsw.edu.au', 
         'abcd1234', 'Bobby', 'Dickens');
-        let quiz = adminQuizCreate(user.userId, name, 'Welcome!');
+        let quiz = adminQuizCreate(user.authUserId, name, 'Welcome!');
         expect(quiz).toStrictEqual( {error: expect.any(String)} );
     });
 
     test('name is already being used', () => {
         let user = adminAuthRegister('quiz@unsw.edu.au', 
         'abcd1234', 'Bobby', 'Dickens');
-        let quiz1 = adminQuizCreate(user.userId, 'COMP1531', 'Welcome!');
-        let quiz2 = adminQuizCreate(user.userId, 'COMP1531', 'Blahblah!');
+        let quiz1 = adminQuizCreate(user.authUserId, 'COMP1531', 'Welcome!');
+        let quiz2 = adminQuizCreate(user.authUserId, 'COMP1531', 'Blahblah!');
         expect(quiz2).toStrictEqual( {error: expect.any(String)} );
     });
 
@@ -100,8 +100,8 @@ describe('adminQuizCreate', () => {
         'abcd1234', 'Bobby', 'Dickens');
         let user2 = adminAuthRegister('quiz2@unsw.edu.au', 
         'abcd1234', 'Sobby', 'Mickens');
-        let quiz1 = adminQuizCreate(user1.userId, 'COMP1531', 'Welcome!');
-        let quiz2 = adminQuizCreate(user2.userId, 'COMP1531', 'BLAHBLAH');
+        let quiz1 = adminQuizCreate(user1.authUserId, 'COMP1531', 'Welcome!');
+        let quiz2 = adminQuizCreate(user2.authUserId, 'COMP1531', 'BLAHBLAH');
         expect(quiz2).toStrictEqual( {quizId: expect.any(Number)} );
     });
 });
@@ -110,30 +110,30 @@ describe('adminQuizRemove', () => {
     beforeEach(() => {
         let user = adminAuthRegister('quiz@unsw.edu.au', 
         'abcd1234', 'Bobby', 'Dickens');
-        let quiz = adminQuizCreate(user.userId, 'COMP1531', 'Welcome!');
+        let quiz = adminQuizCreate(user.authUserId, 'COMP1531', 'Welcome!');
     });
 
     test('successful removal of quiz', () => {
-        expect(adminQuizRemove(user.userId, quiz.quizId)).toEqual( {} );
-        expect(adminQuizList(user.userId)).toStrictEqual({
+        expect(adminQuizRemove(user.authUserId, quiz.quizId)).toEqual( {} );
+        expect(adminQuizList(user.authUserId)).toStrictEqual({
             quizzes: [
             ]
         });
     });
 
     test('authUserId doesnt exist', () => {
-        expect(adminQuizRemove(user.userId + 1, quiz.quizId)).toStrictEqual( {error: expect.any(String)} );
+        expect(adminQuizRemove(user.authUserId + 1, quiz.quizId)).toStrictEqual( {error: expect.any(String)} );
     });
 
     test('quizId doesnt refer to a valid quiz', () => {
-        expect(adminQuizRemove(user.userId, quiz.quizId + 1)).toStrictEqual( {error: expect.any(String)} );
+        expect(adminQuizRemove(user.authUserId, quiz.quizId + 1)).toStrictEqual( {error: expect.any(String)} );
     });
 
     test('quiz doesnt belong to this user', () => {
         let user1 = adminAuthRegister('quiz1@unsw.edu.au', 
         'abcd1234', 'Robby', 'Smith');
-        let quiz1 = adminQuizCreate(user.userId, 'HAHA1531', 'Welcome!');
-        expect(adminQuizRemove(user.userId, quiz.quizId)).toStrictEqual( {error: expect.any(String)});
+        let quiz1 = adminQuizCreate(user.authUserId, 'HAHA1531', 'Welcome!');
+        expect(adminQuizRemove(user.authUserId, quiz.quizId)).toStrictEqual( {error: expect.any(String)});
     });
 });
 
