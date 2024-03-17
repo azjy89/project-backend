acf26aab10b4bba09870aca2d0e88e1722832c04
someguy <andrew.zhang0405@gmail.com>
2024-03-18 01:19:55 +1100

    Made necessary changes to tsconfig.json and dataStore.ts

---
+++ src/dataStore.ts
@@ -0,0 +1,41 @@
+export interface User {
+    userId: number,
+    nameFirst: string,
+    nameLast: string,
+    email: string,
+    password: string,
+    numSuccessfulLogins: number,
+    numFailedPasswordsSinceLastLogin: number,
+    oldPasswords: number[]
+}
+
+export interface Quiz {
+    quizId: number,
+    name: string,
+    quizCreatorId: number,
+    timeCreated: number,
+    timeLastEdited: number,
+    description: string,
+    questions: string[],
+    answers: string[]
+}
+
+export interface Data {
+    users: User[],
+    quizzes: Quiz[]
+}
+
+let data: Data = {
+    users: [],
+    quizzes: []
+};
+
+// Use get() to access the data
+export const getData = (): Data => {
+    return data;
+}
+
+// Use set(newData) to pass in the entire data object, with modifications made
+export const setData = (newData: Data): void => {
+    data = newData;
+}
