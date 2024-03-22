285ed110c35f6bfb2aae475868927faa37711c8f
Put Your Name Here <z5480605@ad.unsw.edu.au>
2024-03-22 16:17:11 +1100

    Finished wrapping iteration 1 functions

--- src/httpRequests.ts
+++ src/httpRequests.ts
@@ -4,7 +4,7 @@ const SERVER_URL = `${url}:${port}`;
 
 // Iteration 1 functions
 
-export const adminAuthRegister = (email: string, password: string, nameFirst: string, nameLast: string) => {
+export const requestAuthRegister = (email: string, password: string, nameFirst: string, nameLast: string) => {
   const res = request(
     'POST',
     SERVER_URL + '/v1/admin/auth/register',
@@ -13,15 +13,15 @@ export const adminAuthRegister = (email: string, password: string, nameFirst: st
         email: email,
         password: password,
         nameFirst: nameFirst,
-        nameLast: nameLast
+        nameLast: nameLast,
       },
     }
   );
   return JSON.parse(res.body.toString());
-}
+};
 
 
-export const adminAuthLogin = (email: string, password: string) => {
+export const requestAuthLogin = (email: string, password: string) => {
   const res = request(
     'POST',
     SERVER_URL + '/v1/admin/auth/login',
@@ -39,7 +39,7 @@ export const adminAuthLogin = (email: string, password: string) => {
 };
 
 
-export const adminUserDetails = (token: string) => {
+export const requestUserDetails = (token: string) => {
   const res = request(
     'GET',
     SERVER_URL + '/v1/admin/user/details',
@@ -52,7 +52,7 @@ export const adminUserDetails = (token: string) => {
   return JSON.parse(res.body.toString());
 };
 
-export const adminUserDetailsUpdate = (token: string, email: string, nameFirst: string, nameLast: string) => {
+export const requestUserDetailsUpdate = (token: string, email: string, nameFirst: string, nameLast: string) => {
   const res = request(
     'PUT',
     SERVER_URL + `/v1/admin/user/details`,
@@ -69,5 +69,126 @@ export const adminUserDetailsUpdate = (token: string, email: string, nameFirst: 
     }
   );
   return JSON.parse(res.body.toString());
-}
+};
+
+export const requestUserPasswordUpdate = (token: string, oldPassword: string, newPassword: string) => {
+  const res = request(
+    'PUT',
+    SERVER_URL + `/v1/admin/user/password`,
+    {
+      body: JSON.stringify({
+        token: token,
+        oldPassword: oldPassword,
+        newPassword: newPassword,
+      }),
+      headers: {
+        'Content-type': 'application/json',
+      }
+    }
+  );
+  return JSON.parse(res.body.toString());
+};
+
+export const requestQuizList = (token: string) => {
+  const res = request(
+    'GET',
+    SERVER_URL + '/v1/admin/quiz/list',
+    {
+      qs: {
+        token: token,
+      }
+    }
+  );
+  return JSON.parse(res.body.toString());
+};
+
+export const requestQuizCreate = (token: string, name: string, description: string) => {
+  const res = request(
+    'POST',
+    SERVER_URL + '/v1/admin/quiz',
+    {
+      body: JSON.stringify({
+        token: token,
+        name: name,
+        description: description,
+      }),
+      headers: {
+        'Content-type': 'application/json',
+      }
+    }
+  );
+  return JSON.parse(String(res.body));
+};
+
+export const requestQuizRemove = (token: string, quizId: number) => {
+  const res = request(
+    'DELETE',
+    SERVER_URL + `/v1/admin/quiz/${quizId}`,
+    {
+      qs: {
+        token: token,
+      },
+    }
+  );
+  return JSON.parse(res.body.toString());
+};
+
+export const requestQuizInfo = (token: string, quizId: number) => {
+  const res = request(
+    'GET',
+    SERVER_URL + `/v1/admin/quiz/${quizId}`,
+    {
+      qs: {
+        token: token,
+      }
+    }
+  );
+  return JSON.parse(res.body.toString());
+};
+
+export const requestQuizNameUpdate = (token: string, quizId: number, name: string) => {
+  const res = request(
+    'PUT',
+    SERVER_URL + `/v1/admin/quiz/${quizId}/name`,
+    {
+      body: JSON.stringify({
+        token: token,
+        name: name,
+      }),
+      headers: {
+        'Content-type': 'application/json',
+      }
+    }
+  );
+  return JSON.parse(res.body.toString());
+};
+
+export const requestQuizDescriptionUpdate = (token: string, quizId: number, description: string) => {
+  const res = request(
+    'PUT',
+    SERVER_URL + `/v1/admin/quiz/${quizId}/description`,
+    {
+      body: JSON.stringify({
+        token: token,
+        description: description,
+      }),
+      headers: {
+        'Content-type': 'application/json',
+      }
+    }
+  );
+  return JSON.parse(res.body.toString());
+};
+
+export const requestClear = () => {
+  const res = request(
+    'DELETE',
+    SERVER_URL + '/v1/clear',
+    {
+      json: {
+
+      }
+    }
+  );
+};
 
