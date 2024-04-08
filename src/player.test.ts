e4010fe9f2d5adaf3ec1c91bce0130407f737473
z5481380 <z5481380@ad.unsw.edu.au>
2024-04-08 22:23:05 +1000

    made player.ts and player.test.ts wrote some preliminary code

---
+++ src/player.test.ts
@@ -0,0 +1,70 @@
+import {
+
+} from './httpRequests';
+
+beforeEach(() => {
+    requestClear();
+  });
+
+
+// have to download moment for unix?
+
+import moment from 'moment';
+
+// Function to format Unix timestamp to a readable date/time string
+function formatUnixTime(timestamp: number): string {
+  return moment.unix(timestamp).format("YYYY-MM-DD HH:mm:ss");
+}
+
+// playerChatSession
+// Goal: Display all chat messages in session
+
+describe('Return all messages in chat', () => {
+    const playerid = playerID('5546');
+    const playerMessages = messages.filter(message => message.playerId === playerId);
+    playerMessages.forEach(message => {
+      console.log(`Message: ${message.messageBody}`);
+      console.log(`Time Sent: ${formatUnixTime(message.timeSent)}`);
+    });
+})
+
+  test('Player ID does not exist', () => {
+    const playerid = playerID('5546');
+    expect(playerChatSession(playerid + 1)).toStrictEqual({ error: expect.any(String) });
+    //return nothing?
+  });
+
+
+// playerChatSend
+// Goal: Send a message into chat
+describe('Sent a message into chat', () => {
+  test('Message sent successfully', () => {
+    const playerid = '5546'; 
+    expect(message)
+});
+});
+
+test('Player ID does not exist', () => {
+  const playerid = playerID('5546');
+  expect(playerChatSession(playerid + 1)).toStrictEqual({ error: expect.any(String) });
+});
+
+
+test('Message body is greater than 100 characters', () => {
+  const playerid = playerID('5546');
+  expect(todo(
+
+    `How much wood can a wood chucker chuck wood? I don't actually know 
+    but that was a great character count filler.`
+  )).toStrictEqual({ error: expect.any(String) });
+});
+
+test('Message body is lesser than 1 character', () => {
+  const playerid = playerID('5546');
+  expect(todo(``)).toStrictEqual({ error: expect.any(String) });
+});
+
+
+
+
+
