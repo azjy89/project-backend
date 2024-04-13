07d9c44e5fd7069e84d71596338d13bfbb027af4
Joseph Wang <z5359179@ad.unsw.edu.au>
2024-04-13 17:32:34 +1000

    Function Implement for playerJoin function.

---
+++ src/playerJoin.ts
@@ -0,0 +1,66 @@
+import { Session } from 'inspector';
+import { getData, getTimerData, setData } from './dataStore';
+import {
+    Data,
+    Player,
+    QuizSession,
+    PlayerId,
+    ErrorObject,
+} from './interfaces';
+import HTTPError from 'http-errors';
+
+// Goal: generate string of 5-letter,3-digit
+const createRandomName = (): string => {
+    var random_string = '';
+    var letters = 'abcdefghijklmnopqrstuvwxyz', numbers = '0123456789'
+    const lettersLength = 5, numbersLength = 3;
+    for (var i = 0; i < lettersLength; i++) {
+        random_string += letters.charAt(Math.floor(Math.random()*lettersLength));
+    };
+    for (var i = 0; i < numbersLength; i++) {
+        random_string += numbers.charAt(Math.floor(Math.random()*numbersLength));
+    };
+    return random_string;
+};
+
+// Goal: Create a new player and add them to the player list (array).
+// Return: playerId
+// QUESTION: Do I still need type "ErrorObject" when I'm throwing the errors?
+export const playerJoin = (sessionId: number, name: string): PlayerId | ErrorObject => {
+    const data: Data = getData();
+    if (name === '') {
+        // create 5-letter,4-digit string
+        name = createRandomName();
+    }
+
+    // ERROR CHECKS:
+    const currQuizSession:QuizSession = data.quizSessions.find(session => session.sessionId === sessionId);
+    if (!currQuizSession) {
+        throw HTTPError(400, 'Session Id does not refer to a valid session');
+    }
+    const searchName = currQuizSession.players.find(player => player.name === name);
+    if (searchName) {
+        throw HTTPError(400, 'Name of user entered is not unique (compared to other users who have already joined)');
+    }
+    // FIX: IS THERE ANOTHER WAY TO WRITE LOBBY (ENUM INDEX: 0)?
+    if (currQuizSession.state !== 0) {
+        throw HTTPError(400, 'Session is not in LOBBY state');
+    }
+
+    // CREATE NEW PLAYER: (Note: "state: 0" is enum LOBBY.)
+    const newPlayerId = currQuizSession.players.length;
+    const newPlayer: Player = {
+        playerId: newPlayerId,
+        name: name,
+        score: 0,
+        state: 0, 
+        numQuestions: 0,
+        atQuestion: 0,
+    }
+    currQuizSession.players.push(newPlayer);
+
+    setData(data);
+    return { 
+        playerId: newPlayerId
+    };
+};
